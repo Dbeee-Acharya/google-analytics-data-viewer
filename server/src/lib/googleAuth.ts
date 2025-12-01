0117274b195ba37d9128bf4c697a3f8560eecb68
Dibbeshwor Acharya <dibbeshwor@gmail.com>
2025-12-01 11:03:21 +0545

    change arcitecture

--- server/src/lib/googleAuth.ts
+++
@@ -1,9 +0,0 @@
-import { GoogleAuth } from "google-auth-library";
-import config from "../config/config.js";
-
-const auth = new GoogleAuth({
-  keyFile: config.serviceFilePath,
-  scopes: ['https://www.googleapis.com/auth/analytics.readonly'],
-});
-
-export default auth;
